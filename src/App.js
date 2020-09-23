93b7f7d03062ab3c349825a752e2386354f8e4c0
liyixuan <13069579956@163.com>
2020-09-23 12:46:57 +0800

    AboutMe模块

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Hello from './components/Hello';
+import AboutMe from './components/AboutMe';
+
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Hello />
+        <AboutMe />
       </div>
     );
   }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/AboutMe.scss';
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3 className="hello">AboutMe</h3>
+        <p className="aboutMe">
+          Lorem ipsum dolor sit amet,consectetur adipisicing elit.Pepellendus,
+          non,dolorem,cumque distinctio magni quam expedita velit laborum sunt
+          amet facere temora ut fuga aliquam ad asperiores volatatem dolorum!
+          Quasi.
+        </p>
+      </div>
+    );
+  }
+}
+export default AboutMe;
